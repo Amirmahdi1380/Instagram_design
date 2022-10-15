6e2b9f5a1f7b6a4e63e1eea321b96b2666349aa4
Amirmahdi Nourkazemi <amirmahdi@Amirmahdis-MacBook-Pro.local>
2022-10-15 15:28:20 +0330

    login page design compelete

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,33 +1,201 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  FocusNode negahban1 = FocusNode();
+  FocusNode negahban2 = FocusNode();
+  @override
+  void initState() {
+    super.initState();
+    negahban1.addListener(() {
+      setState(() {});
+    });
+    negahban2.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xff323A99),
+            Color(
+              0xffF98BFC,
+            ),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            _getImage(),
+            _getContainer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getImage() {
+    return Positioned(
+      top: 50,
+      bottom: 0,
+      right: 0,
+      left: 0,
+      child: Column(
         children: [
-          Positioned(
-            top: 88,
-            child: Expanded(
-              child: Image(
-                image: AssetImage('assets/images/rocket2 1.png'),
-              ),
+          Expanded(
+            child: Image(
+              image: AssetImage('assets/images/rocket2 1.png'),
             ),
           ),
           Expanded(
-            child: Container(
-              height: 428,
-              width: 461,
-              decoration: BoxDecoration(
-                color: Color(0xff1C1F2E),
-                borderRadius: BorderRadius.circular(15),
-              ),
-            ),
-          ),
+            child: Container(),
+          )
         ],
       ),
     );
+  }
+
+  Widget _getContainer() {
+    return Column(
+      children: [
+        Expanded(
+          child: Container(),
+        ),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xff1C1F2E),
+              borderRadius: BorderRadius.circular(
+                15,
+              ),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'sign in',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'GB',
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: 36,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 44),
+                  child: TextField(
+                    focusNode: negahban1,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        color: negahban1.hasFocus
+                            ? Color(0xffF35383)
+                            : Colors.white,
+                        fontFamily: 'GM',
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(
+                          color: Color(0xffC5C5C5),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(
+                          color: Color(0xffF35383),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 44),
+                  child: TextField(
+                    focusNode: negahban2,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        color: negahban2.hasFocus
+                            ? Color(0xffF35383)
+                            : Colors.white,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(
+                          color: Color(0xffF35383),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(
+                          color: Color(0xffC5C5C5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 45,
+                ),
+                SizedBox(
+                  width: 129,
+                  height: 46,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'sign in',
+                      style: TextStyle(fontFamily: 'GB', fontSize: 16),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    negahban1.dispose();
+    negahban2.dispose();
+    super.dispose();
   }
 }
