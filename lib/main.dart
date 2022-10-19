7ae65e83ce1fd3121ce8bb0a578f8b6b5f9c8612
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2022-10-19 12:37:01 +0330

    add picture

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_application/screens/home_screen.dart';
 import 'package:instagram_application/screens/login_page.dart';
+import 'package:instagram_application/screens/switch_account_screen.dart';
 
 void main() {
   runApp(Application());
@@ -31,7 +32,7 @@ class Application extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: switchAccount(),
     );
   }
 }
